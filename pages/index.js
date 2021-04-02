d7f79a8f2793eb204855fd4401a5b5c8dd2eae52
Robert Reinert <robert.reinert@gmail.com>
2021-04-02 11:40:10 -0700

    form cleanup and onSubmit to post to api added

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,24 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const addCustomer = async e => {
+    e.preventDefault();
+    const res = await fetch('/api/submissions', {
+      method: 'POST',
+      body: JSON.stringify({
+        first_name: e.target.first_name.value,
+        last_name: e.target.last_name.value,
+        email: e.target.email.value,
+        phone: e.target.phone.value,
+        zip: e.target.zip.value,
+      })
+    });
+    const json = await res.json();
+    if(json.errors) {
+      console.error(json.errors)
+      throw new Error('Failed to fetch API');
+    }
+  }
   return (
     <div className={styles.container}>
       <Head>
@@ -20,21 +38,20 @@ export default function Home() {
         </p>
         
         <div>
-          <form>
-            First Name: <input type="text" name="first_name" /><br/>
-            
-            Last Name: <input type="text" name="last_name" /><br/>
-            
-            Email: <input type="email" name="email" /><br/>
-
-            Phone: <input type="tel" name="phone" /><br/>
-
-            Zip: <input type="text" name="zip" /><br/>
-
+          <form onSubmit={addCustomer} method="POST">
+            <label htmlFor="first_name">First Name:</label>
+            <input type="text" name="first_name" required /><br/>
+            <label htmlFor="last_name">Last Name:</label>
+            <input type="text" name="last_name" required /><br/>
+            <label htmlFor="email">Email:</label>
+            <input type="email" name="email" required /><br/>
+            <label htmlFor="phone">Phone:</label>
+            <input type="tel" name="phone" required /><br/>
+            <label htmlFor="zip">Zip:</label>
+            <input type="text" name="zip" required pattern="\d{5}"/><br/>
             <button type="submit">Submit</button>
           </form>
         </div>
-
       </main>
     </div>
   )
