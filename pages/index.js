ef305799275f049ed5b675f9b903ccc7ca2fcfcf
Robert Reinert <robert.reinert@gmail.com>
2021-04-02 17:17:09 -0700

    handling form values with state

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,11 @@ import React, { useState, useEffect } from 'react'
 
 export default function Home() {
   const [ formResMessage, setFormResMessage ] = useState([]);
+  const [ firstName, setFirstName ] = useState('');
+  const [ lastName, setLastName ] = useState('');
+  const [ email, setEmail ] = useState('');
+  const [ phone, setPhone ] = useState('');
+  const [ zip, setZip ] = useState('');
   const addCustomer = async e => {
     e.preventDefault();
     const res = await fetch('/api/submissions', {
@@ -12,11 +17,11 @@ export default function Home() {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        first_name: e.target.first_name.value,
-        last_name: e.target.last_name.value,
-        email: e.target.email.value,
-        phone: e.target.phone.value,
-        zip: e.target.zip.value,
+        first_name: firstName,
+        last_name: lastName,
+        email: email,
+        phone: phone,
+        zip: zip,
       })
     });
     const json = await res.json();
@@ -24,50 +29,45 @@ export default function Home() {
       console.error(json.errors)
       throw new Error('Failed to fetch API');
     }
-    switch (json.status) {
-      case 'unavailable':
-        setFormResMessage([json]);
-        break;
-      case 'error':
-        setFormResMessage([json]);
-        break;
-      default:
-        setFormResMessage([json]);
-    }
+    setFormResMessage([json]);
+    setFirstName('');
+    setLastName('');
+    setEmail('');
+    setPhone('');
+    setZip('');
   }
+
   return (
     <div >
       <Head>
         <title>EnergyPal Take-Home Test</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main className="w-screen h-screen flex items-center justify-center bg-gray-50">
         <div className="w-screen mx-4 sm:w-10/12 md:w-6/12 lg:w-96 md:border md:rounded-xl md:p-5 md:bg-white">
         <h1 className="text-3xl font-semibold text-gray-800 mb-8">
           Welcome to <a className="text-blue-500" href="https://energypal.com">EnergyPal!</a>
         </h1>
-
           <form className="grid grid-cols-1 gap-3" onSubmit={addCustomer} method="POST">
             <label htmlFor="first_name">
               <span className="text-xs font-medium text-gray-700" >First Name</span>
-              <input className="border rounded text-gray-700 placeholder-gray-400 focus:ring-2 focus:ring-blue-500 focus:border-transparent focus:outline-none w-full p-2" id="first_name" type="text" name="first_name" required placeholder="Your first name"/><br/>
+              <input onChange={e => {setFirstName(e.target.value)}} value={firstName} className="border rounded text-gray-700 placeholder-gray-400 focus:ring-2 focus:ring-blue-500 focus:border-transparent focus:outline-none w-full p-2" id="first_name" type="text" name="first_name" required placeholder="Your first name"/><br/>
             </label>
             <label htmlFor="last_name">
               <span className="text-xs font-medium text-gray-700" >Last Name</span>
-              <input className="border rounded text-gray-700 placeholder-gray-400 focus:ring-2 focus:ring-blue-500 focus:border-transparent focus:outline-none w-full p-2"  id="last_name" type="text" name="last_name" required placeholder="Your last name"/><br/>
+              <input onChange={e => {setLastName(e.target.value)}} value={lastName} className="border rounded text-gray-700 placeholder-gray-400 focus:ring-2 focus:ring-blue-500 focus:border-transparent focus:outline-none w-full p-2"  id="last_name" type="text" name="last_name" required placeholder="Your last name"/><br/>
             </label>
             <label htmlFor="email">
               <span className="text-xs font-medium text-gray-700" >Email</span>
-              <input className="border rounded text-gray-700 placeholder-gray-400 focus:ring-2 focus:ring-blue-500 focus:border-transparent focus:outline-none w-full p-2"  id="email" type="email" name="email" required placeholder="Ex. billgates@microsoft.com"/><br/>
+              <input onChange={e => {setEmail(e.target.value)}} value={email} className="border rounded text-gray-700 placeholder-gray-400 focus:ring-2 focus:ring-blue-500 focus:border-transparent focus:outline-none w-full p-2"  id="email" type="email" name="email" required placeholder="Ex. billgates@microsoft.com"/><br/>
             </label>
             <label htmlFor="phone">
               <span className="text-xs font-medium text-gray-700" >Phone</span>
-              <input className="border rounded text-gray-700 placeholder-gray-400 focus:ring-2 focus:ring-blue-500 focus:border-transparent focus:outline-none w-full p-2"  id="phone" type="tel" name="phone" required placeholder="Your phone number"/><br/>
+              <input onChange={e => {setPhone(e.target.value)}} value={phone} className="border rounded text-gray-700 placeholder-gray-400 focus:ring-2 focus:ring-blue-500 focus:border-transparent focus:outline-none w-full p-2"  id="phone" type="tel" name="phone" required placeholder="Your phone number"/><br/>
             </label>
             <label htmlFor="zip">
               <span className="text-xs font-medium text-gray-700" >Zip</span>
-              <input className="border rounded text-gray-700 placeholder-gray-400 focus:ring-2 focus:ring-blue-500 focus:border-transparent focus:outline-none w-full p-2"  id="zip" type="text" name="zip" required pattern="\d{5}" placeholder="90210"/><br/>
+              <input onChange={e => {setZip(e.target.value)}} value={zip} className="border rounded text-gray-700 placeholder-gray-400 focus:ring-2 focus:ring-blue-500 focus:border-transparent focus:outline-none w-full p-2"  id="zip" type="text" name="zip" required pattern="\d{5}" placeholder="90210"/><br/>
             </label>
             <div className="flex justify-end mt-4">
               <button className="bg-blue-500 hover:bg-blue-700 rounded text-white w-full md:w-auto py-2 md:px-6 shadow-md" type="submit">Submit</button>
@@ -75,17 +75,18 @@ export default function Home() {
             {formResMessage.map(i => {
               if (i.status === 'unavailable' || i.status === 'error') {
                 return <div className="text-red-700 text-sm flex items-center mt-4 w-full justify-center">
-                    <svg class="inline" xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20" fill="currentColor">
-                      <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd"></path>
-                    </svg>
-                    <span className="ml-1">{i.message}</span>
-                  </div>
+                  <svg class="inline" xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd"></path>
+                  </svg>
+                  <span className="ml-1">{i.message}</span>
+                </div>
               } else {
                 return <div className="text-green-700 text-sm flex items-center mt-4 w-full justify-center">
                   <svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20" fill="currentColor">
                     <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
                   </svg>
-                  <span className="ml-1">{i.message}</span></div>
+                  <span className="ml-1">{i.message}</span>
+                </div>
               }
             })
           }
