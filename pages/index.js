0f9da2c403c1cb2fd8a8a2a8d177abe0d5d23b3f
Chris <chris@welcometocbs.com>
2023-01-26 18:40:54 -0500

    Added Styling from design.png

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
+
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -8,7 +9,71 @@ export default function Home() {
         <title>EnergyPal Take-Home Test</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <main className={styles.main}>
+        <div className='grid lg:grid-cols-2 gap-12'>
+          <div>
+            <div>
+              <h1 className={styles.title}>
+                Get Started
+              </h1>
+            </div>
+            <div className='py-3'>
+              <p className={styles.description}>
+                Speak to an EnergyPal advisor about our current deals on solar panels and home batteries.
+              </p>
+            </div>
+          </div>
+          <div className={styles.form}>
+            <form>
+              <div className='grid grid-cols-1 gap-y-6'>
+                <div className='grid md:grid-cols-2 gap-y-6 gap-x-12'>
+                  <div className='grid grid-cols-1'>
+                    <label for="fname">First Name</label>
+                    <input id="fname" type="text" name="first_name" className='rounded-3xl px-3 py-2 border border-black/20 outline-blue-700' />
+                  </div>
+                  <div className='grid grid-cols-1'>
+                    <label for="lname">Last Name</label>
+                    <input id="lname" type="text" name="last_name" className='rounded-3xl px-3 py-2 border border-black/20 outline-blue-700' />
+                  </div>
+                  <div className='grid grid-cols-1'>
+                    <label for="email">Email Address</label>
+                    <input id="email" type="email" name="email" className='rounded-3xl px-3 py-2 border border-black/20 outline-blue-700' />
+                  </div>
+                  <div className='grid grid-cols-1'>
+                    <label for="phone">Phone Number</label>
+                    <input id="phone" type="tel" name="phone" className='rounded-3xl px-3 py-2 border border-black/20 outline-blue-700' />
+                  </div>
+                </div>
+                <div className='grid grid-cols-1'>
+                  <label for="zip">Postal Code</label>
+                  <input id="zip" type="text" name="zip" className='rounded-3xl px-3 py-2 border border-black/20 outline-blue-700' />
+                </div>
+              </div>
+              <div className='py-12 flex justify-center'>
+                <button className='rounded-full bg-blue-700 py-3 px-32 text-white text-xl font-semibold' type="submit">Submit</button>
+              </div>
+              <div>
+                <p className='text-sm'>Thanks for your interest in EnergyPal! By clicking above, you agree we may call and text you about EnergyPal products at the number provided even if on a "do not call" list, using pre-recorded messages or autodialing. Msg and data rates may apply. Your consent is optional, opt-out anytime.</p>
+              </div>
+            </form>
+          </div>
+        </div>
+      </main>
+      <footer>
+        <p className='text-sm pb-2'>&copy; Copyright {new Date().getFullYear()} EnergyPal.com, All Rights Reserved. <a href="">Privacy Policy.</a> <a href="">Terms of Service.</a></p>
+      </footer>
+    </div>
+  )
+}
 
+//* ORIGINAL HOME USED FOR REFERENCE
+export function Home2() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>EnergyPal Take-Home Test</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://energypal.com">EnergyPal!</a>
@@ -18,18 +83,18 @@ export default function Home() {
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
-        
+
         <div>
           <form>
-            First Name: <input type="text" name="first_name" /><br/>
-            
-            Last Name: <input type="text" name="last_name" /><br/>
-            
-            Email: <input type="email" name="email" /><br/>
+            First Name: <input type="text" name="first_name" /><br />
 
-            Phone: <input type="tel" name="phone" /><br/>
+            Last Name: <input type="text" name="last_name" /><br />
 
-            Zip: <input type="text" name="zip" /><br/>
+            Email: <input type="email" name="email" /><br />
+
+            Phone: <input type="tel" name="phone" /><br />
+
+            Zip: <input type="text" name="zip" /><br />
 
             <button type="submit">Submit</button>
           </form>
