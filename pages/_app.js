6b2e28d3dfd5fd6c7edd0bc63fd7338cbe268c7e
Kei Kaloustian <kei.kaloustian@gmail.com>
2023-03-02 16:37:39 -0800

    Set up Roboto web font w/ built-in self hosting

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { Roboto } from "@next/font/google";
+
+const roboto = Roboto({
+  weight: ["300", "400"],
+  subsets: ["latin"],
+  variable: "--font-roboto",
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${roboto.variable} font-sans`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
 
-export default MyApp
+export default MyApp;
